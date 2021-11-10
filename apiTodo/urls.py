31c3c09c38fc5569ad1ea2545ceb47e1165a0da9
C8338Soner <sonerpeker84@gmail.com>
2021-11-10 21:59:27 +0300

    Concrete Views

--- apiTodo/urls.py
+++ apiTodo/urls.py
@@ -1,14 +1,33 @@
 from django.urls import path
-from .views import home, todoList, todoListCreate, toDo_list, todoListUpdate, todoListDelete, toDo_detail
+from .views import (home,
+                    todoList,
+                    todoListCreate,
+                    toDo_list,
+                    todoListUpdate,
+                    todoListDelete,
+                    toDo_detail,
+                    TodoList,
+                    TodoDetail,
+                    TodoListCreate,
+                    TodoRetrieveUpdateDelete,
+                    TodoConcListCreate,
+                    TodoConcRetreiveUpdateDelete
+                    )
 
 
 urlpatterns = [
     path('', home),
-    path('list/', todoList), 
-    path('create/', todoListCreate),   
-    path('todo/', toDo_list), 
-    path('update/<int:pk>/', todoListUpdate),  
-    path('delete/<int:pk>/', todoListDelete),    
-    path('detail/<int:pk>/', toDo_detail),  
-    
+    # path('todoList/', todoList),
+    # path('todoListCreate/', todoListCreate),
+    # path('toDo_list/', toDo_list),
+    # path('todoListUpdate/<int:pk>/', todoListUpdate),
+    # path('todoListDelete/<int:pk>/', todoListDelete),
+    # path('toDo_detail/<int:pk>/', toDo_detail),
+    # path('todo-list/', TodoList.as_view()),
+    # path('todo-list/', TodoListCreate.as_view()),
+    path('todo-list/', TodoConcListCreate.as_view()),
+    # path('todo-detail/<int:pk>', TodoDetail.as_view()),
+    # path('todo-detail/<int:pk>', TodoRetrieveUpdateDelete.as_view()),
+    path('todo-detail/<int:pk>', TodoConcRetreiveUpdateDelete.as_view()),
+
 ]
